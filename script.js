f524bda67bbb7288d88d2c9d662ba054a713944b
Snehasish Roy <105574399+snehasishxd@users.noreply.github.com>
2022-08-15 11:41:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {new TypeIt("#type", {strings: ["A Space Enthusiast and I Code with JavaScript (MERN) :^)"],}).go();});
+document.addEventListener("DOMContentLoaded", function () {new TypeIt("#type", {strings: ["A Space Enthusiast and I like to code and do random stuff UwU"],}).go();});
